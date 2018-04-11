bb30ea6e50d847f8e4ad498ca18e0166ef2afe13
Oleg Bogomaz <obogomaz@medgate.com>
2018-04-11 12:05:21 -0400

    update

--- Web/Web/Startup.cs
+++ Web/Web/Startup.cs
@@ -52,7 +52,9 @@ namespace Web
             app.Use(async (context, next) =>
                 {
                     await next();
-                    if (context.Response.StatusCode == 404 && !Path.HasExtension(context.Request.Path.Value))
+                    if (context.Response.StatusCode == 404 &&
+                        !Path.HasExtension(context.Request.Path.Value) &&
+                        !context.Request.Path.Value.StartsWith("/api/"))
                     {
                         context.Request.Path = "/index.html";
                         await next();
